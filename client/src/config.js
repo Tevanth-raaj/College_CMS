a4155286446673d67109610079fc95fdc39b7224
Sanjeevi Rajan <>
2026-03-06 10:55:03 +0530

    Fix GOOGLE_CLIENT_ID assignment to ensure proper fallback to environment variable

--- client/src/config.js
+++ client/src/config.js
@@ -4,6 +4,6 @@ const API_BASE_URL =
   "http://localhost:5000/api"
 
 const GOOGLE_CLIENT_ID =
-  process.env.REACT_APP_GOOGLE_CLIENT_ID || ""
+  "880469513355-ndrhbbo7m85kete7010vj4fh3nabc3g2.apps.googleusercontent.com" || process.env.REACT_APP_GOOGLE_CLIENT_ID
 
 export { API_BASE_URL, GOOGLE_CLIENT_ID };
